cad746ec5863b77475e4705ea068a48a0c0192e1
Marco Berardini <mberardini@evolution-man.com>
2019-10-16 19:13:43 -0700

    Added comments

--- javascript/twoSum.js
+++ javascript/twoSum.js
@@ -1,12 +1,16 @@
-var twoSum = function (nums, target) {
-  const hashMap = {};
-  for (let i = 0; i < nums.length; i++) {
-    const complement = target - nums[i];
-    if (hashMap[nums[i]] === undefined) {
-      hashMap[nums[i]] = i;
+var twoSum = function(nums, target) {
+   const obj = {};
+    for (let i = 0; i < nums.length; i++) {
+        // calculate complement
+        const complement = target - nums[i];
+        // if it doesn't exist in the obj add it
+        if (obj[nums[i]] === undefined) {
+          obj[nums[i]] = i;
+        }
+        // check that the complement exists and that the numbers are not at the same index
+        // this is to prevent [3] , 6 index 0 counting as both number and compliment
+        if( obj[complement] !== undefined && obj[complement] !== i) {
+            return [obj[complement], i]
+        }
     }
-    if (hashMap[complement] !== undefined && hashMap[complement] !== i) {
-      return [hashMap[complement], i]
-    }
-  }
 };
